366f7a2c157f1d2885228ea84614276f9dbc8e3c
Ahsan Ali Shah <ahsanali.bscssef20@iba-suk.edu.pk>
2024-02-06 00:48:03 +0500

    added middleware

--- server.js
+++ server.js
@@ -18,8 +18,59 @@ const users = [
 
 ]
 
-app.get("/users", (req, res)=>{
-    res.json(users);
+const posts = [
+    {id: 1, name: "Post 1"},
+    {id: 2, name: "Post 2"},
+    {id: 3, name: "Post 3"},
+    {id: 4, name: "Post 4"},
+    {id: 5, name: "Post 5"},
+    {id: 6, name: "Post 6"},
+    {id: 7, name: "Post 7"},
+    {id: 8, name: "Post 8"},
+    {id: 9, name: "Post 9"},
+    {id: 10, name: "Post 10"},
+    {id: 11, name: "Post 11"},
+    {id: 12, name: "Post 12"},
+    {id: 13, name: "Post 13"},
+
+]
+
+app.get("/users", paginatedResults(users), (req, res)=>{
+    res.json(res.paginatedResults)
 })
+
+app.get("/posts", paginatedResults(posts), (req, res)=>{
+    res.json(res.paginatedResults)
+})
+
+function paginatedResults(model){
+    return (req, res, next) => {
+        const page = parseInt(req.query.page)
+        const limit = parseInt(req.query.limit)
+
+        const startIndex = (page-1)*limit
+        const endIndex = page * limit
+
+        const results = {}
+
+        if(endIndex < model.length){
+            results.next = {
+                page: page + 1,
+                limit: limit
+            }
+        }
+
+        if(startIndex > 0){
+            results.previous = {
+                page: page - 1,
+                limit: limit
+            }
+        }
+        results.results = model.slice(startIndex, endIndex)
+
+        res.paginatedResults = results;
+        next();
+    }
+}
 
 app.listen(3000);
